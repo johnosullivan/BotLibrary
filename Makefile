1a884787e134a81b0efdbe287af3a33d5fe2bc0b
John O'Sullivan <josullivan1@luc.edu>
2017-02-03 23:29:47 -0600

    src

---
+++ Makefile
@@ -0,0 +1,52 @@
+BASIC_CFLAGS = -Wall -O3 -fPIC -g -std=c99 -pedantic -Ilua_src
+
+ALL_CFLAGS = $(BASIC_CFLAGS) $(CFLAGS)
+
+PREFIX = /usr/local
+RM = rm -f
+INSTALL = install -p
+INSTALL_EXEC = $(INSTALL) -m 0755
+INSTALL_DATA = $(INSTALL) -m 0644
+LUA_VERSION = 5.3
+MODULE_NAME = rolib
+
+uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
+
+ifeq ($(uname_S), Darwin)
+	SHARELIB_FLAGS = -dynamiclib -Wl,-undefined,dynamic_lookup
+else
+	SHARELIB_FLAGS = --shared
+endif
+
+all: $(MODULE_NAME).so
+
+LIB_H += $(wildcard *.h)
+
+OBJECTS += src/rolib.o
+
+$(OBJECTS): $(LIB_H)
+
+$(OBJECTS): %.o: %.c
+	$(CC) -o $*.o -c $(ALL_CFLAGS) $<
+
+$(MODULE_NAME).so: $(OBJECTS)
+	$(CC) $(SHARELIB_FLAGS) -o $@ $^
+
+install: all
+	$(INSTALL_DATA) $(MODULE_NAME).so $(PREFIX)/lib/lua/$(LUA_VERSION)/$(MODULE_NAME).so
+
+uninstall:
+	$(RM) $(PREFIX)/lib/lua/$(LUA_VERSION)/$(MODULE_NAME).so
+
+test: all
+	lua tests/demo.lua
+
+tags:
+	find . \( -name .git -type d -prune \) -o \( -name '*.[hc]' -type f -print \) | xargs ctags -a
+
+clean:
+	$(RM) $(MODULE_NAME).so
+	$(RM) -r $(MODULE_NAME).so.dSYM
+	$(RM) $(OBJECTS)
+
+.PHONY: all install uninstall clean test tags
---
+++ src/rolib.c
@@ -0,0 +1,76 @@
+/*
+ Copyright 2016 John O'Sullivan
+
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the
+ "Software"), to deal in the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish,
+ distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to
+ the following conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#define lrolib_c
+#define VERSIONROLIB (l_mathop(1.0))
+
+/* Linking Frameworks */
+#include <lua.h>
+#include <lauxlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <math.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#ifdef _WIN32
+#define O_NOCTTY 0
+#else
+#include <termios.h>
+#endif
+
+/* Defining Vars */
+#undef PI
+#define PI  (l_mathop(3.141592653589793238462643383279502884))
+#if !defined(l_rand)
+#if defined(LUA_USE_POSIX)
+#define l_rand()    random()
+#define l_srand(x)  srandom(x)
+#define L_RANDMAX   2147483647
+#else
+#define l_rand()    rand()
+#define l_srand(x)  srand(x)
+#define L_RANDMAX   RAND_MAX
+#endif
+#endif
+
+/* Will list what feature are supported on machine */
+static int version (lua_State *L) {
+    lua_pushnumber(L, VERSIONROLIB);
+    return 1;
+}
+/* Library functions */
+static const struct luaL_Reg lservo_functions[] = {
+    { "version",                       version                           },
+    { NULL,                            NULL                              }
+};
+/* Init the Lua Robot Library */
+LUAMOD_API int luaopen_rolib (lua_State *L) {
+    luaL_newlib(L, lservo_functions);
+    return 1;
+}
